6d53b2315723265ce13ac24fb6dd42b6e78c5d23
Eric White <erwhite99-git@yahoo.com>
2019-10-15 23:16:02 -0500

    Enable pyenv and emacs pyvenv automatic integration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,15 +6,70 @@
 ;; Misc personal settings
 (require 'init-settings)
 
+;; (defun pyenv-activate-current-project ()
+;;   "Automatically activates pyenv version if .python-version file exists."
+;;   (interactive)
+;;   (let ((python-version-directory (locate-dominating-file (buffer-file-name) ".python-version")))
+;;     (if python-version-directory
+;;         (let* ((pyenv-version-path (f-expand ".python-version" python-version-directory))
+;;                (pyenv-current-version (s-trim (f-read-text pyenv-version-path 'utf-8))))
+;;           (pyenv-mode-set pyenv-current-version)
+;;           (message (concat "Setting virtualenv to " pyenv-current-version))))))
+
+
+;; (defvar pyenv-current-version nil nil)
+
+;; (defun pyenv-init()
+;;   "Initialize pyenv's current version to the global one."
+;;   (let ((global-pyenv (replace-regexp-in-string "\n" "" (shell-command-to-string "pyenv global"))))
+;;     (message (concat "Setting pyenv version to " global-pyenv))
+;;     (pyenv-mode-set global-pyenv)
+;;     (setq pyenv-current-version global-pyenv)))
+
+;; (add-hook 'after-init-hook 'pyenv-init)
 (use-package elpy
   :ensure t
+  :defer t
   :init
-  (elpy-enable))
+  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+  ;; pyvenv config for pyenv
+  (defun workon-auto()
+    (setq-local pyvenv-workon (string-trim (shell-command-to-string "pyenv version-name")))
+    (pyvenv-workon pyvenv-workon)
+    )
+;;  (add-to-list 'exec-path "~/.pyenv/shims")
+  (setenv "WORKON_HOME" "~/.pyenv/versions/")
+  (add-hook 'python-mode-hook 'workon-auto)
+  (add-hook 'hy-mode-hook 'workon-auto)
+  ;; end pyvenv config
+  (advice-add 'python-mode :before 'elpy-enable)
+  :config
+  (setq elpy-rpc-backend "jedi")
+  (pyvenv-tracking-mode 1) ;; pyvenv config
+  )
 
-(use-package company-quickhelp
-  :ensure t
-  :init
-  (company-quickhelp-mode))
+;; (use-package pyenv-mode
+;;   :init
+;;   (add-to-list 'exec-path "~/.pyenv/shims")
+;;   (setenv "WORKON_HOME" "~/.pyenv/versions/")
+;;   :config
+;;   (pyenv-mode))
+;;   ;; :bind
+;;   ;; ("C-x p e" . pyenv-activate-current-project))
+
+;; (use-package python
+;;   :mode ("\\.py" . python-mode)
+;;   :config
+;;   (setq python-indent-offset 4)
+;;   (elpy-enable))
+
+;; (use-package company-quickhelp
+;;   :ensure t
+;;   :init
+;;   (company-quickhelp-mode))
+
+;; (use-package pyenv-mode-auto
+;;   :ensure t)
 
 (require 'init-theme)
 
--- emacs.d/lisp/init-package.el
+++ emacs.d/lisp/init-package.el
@@ -1,8 +1,18 @@
 ;; use-package setup
 (require 'package)
 (setq package-enable-at-startup nil) ; dont do it immediately
-(setq package-archives '(
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(setq package-archives
+      '(
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("melpa"        . 0)))
+
+;; Work around emacs 26.1 bug
+;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (package-initialize)
 
 ;; Bootstap use-package
